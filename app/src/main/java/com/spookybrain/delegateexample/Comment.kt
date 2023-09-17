91fe2ac2df78d9b89071c3cad6403bf9e061c02e
Alexis Tamariz <alexis.tamher@gmail.com>
2023-09-17 01:11:56 -0600

    basic comments list behavior created

---
+++ app/src/main/java/com/spookybrain/delegateexample/Comment.kt
@@ -0,0 +1,10 @@
+package com.spookybrain.delegateexample
+
+data class Comment(
+    val avatar: String,
+    val name: String,
+    val content: String,
+    val created: String,
+    val liked: Int,
+    val shared: Int
+)
--- app/src/main/java/com/spookybrain/delegateexample/MainActivity.kt
+++ app/src/main/java/com/spookybrain/delegateexample/MainActivity.kt
@@ -2,10 +2,31 @@ package com.spookybrain.delegateexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: CommentListAdapter
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.rvCommentList)
+
+        adapter = CommentListAdapter(
+            listOf(
+                Comment(
+                    "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/261.jpg",
+                    "Doyle Sherwood",
+                    "Quo error non omnis quisquam. Hic dolore reprehenderit debitis neque. Nobis libero cupiditate ipsa voluptas veritatis porro ab maiores. Quos rem dolores natus delectus.",
+                    "2023-09-17T00:42:38.281-06:00",
+                    0,
+                    3
+                )
+            )
+        )
+
+        recyclerView.adapter = adapter
     }
 }
